ffbd815ff23af65f31dfbcdae78ee120bce60b6c
Jake Lovitz <jakelovitz@pipes-mbp.mynetworksettings.com>
2023-09-09 18:06:46 -0400

    restructured file layout to avoid code redundancies

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import ReactDOM from "react-dom/client";
 import { Route, Routes } from "react-router-dom"
 import logo from './logo.svg';
 import './styling.css';
-import { default as HomePage } from './homepage.js';
+// import { default as HomePage } from './homepage.js';
 import { default as Header } from "./Header.js";
 import { default as TestingPage } from './TestingPage.js';
 import { default as AboutMePage } from './AboutMePage.js';
+import { default as PageFrame } from './PageFrame.js';
 
 export function App() {
   return (
     
     <Routes>
-      <Route path="/" element={ <HomePage />} />
+      <Route path="/" element={ <PageFrame />} />
       <Route path="/testingpage" element={<TestingPage />} />
       <Route path="/AboutPipes" element={< AboutMePage/>} />
     </Routes>
