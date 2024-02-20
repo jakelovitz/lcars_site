dbd7bccd4a2ecff9b8c73a18fc0562687547dd41
Jake Lovitz <jakelovitz@pipes-mbp.mynetworksettings.com>
2024-02-19 21:50:38 -0500

    Reimplemented the library page and made some additional updates to the site structure

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,11 +3,16 @@ import React from 'react';
 const Book = ({ Title, Subtitle, Series, Author, AuthorLastFirst, YearPublished, Genre, NumberOfPages, Tags }) => {
   return (
     <div className="book">
+
+        {/* <div class="lcars-text-bar">
+            <span>{Title}</span>
+        </div> */}
+
       <h2>{Title}</h2>
       {Subtitle && <h4>{Subtitle}</h4>}
       <p>Author: {Author}</p>
-      {YearPublished &&       <p>Year Published: {YearPublished}</p>}
       <p>Genre: {Genre}</p>
+      {YearPublished && <p>Year Published: {YearPublished}</p>}
       {NumberOfPages && <p>Number of Pages: {NumberOfPages}</p>}
       {Tags && <p>Tags: {Tags}</p>}
     </div>
--- src/pages/LibraryPage.js
+++ src/pages/LibraryPage.js
@@ -1,23 +1,54 @@
-import React from 'react';
-import useFetchAndParseBookList from './../hooks/useFetchAndParseBookList';
+import React, { Component } from 'react';
+import fetchAndParseBookList from '../utils/useFetchAndParseBookList';
 import Book from './../components/Book'; // Adjust the import path as necessary
 
-const LibraryPage = () => {
-  const { books, loading, error } = useFetchAndParseBookList();
-
-  if (loading) return <div>Loading books...</div>;
-  if (error) return <div>Error loading books: {error.message}</div>;
-
-  return (
-    <div>
-      <h1>Library</h1>
-      <div className="book-list">
-        {books.map((book, index) => (
-          <Book key={index} {...book} />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default LibraryPage;
+class LibraryPage extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        books: [],
+        loading: true,
+        error: null
+      };
+    }
+  
+    componentDidMount() {
+      this.fetchBooks();
+    }
+  
+    fetchBooks = () => {
+      fetchAndParseBookList()
+        .then(books => {
+          this.setState({
+            books: books,
+            loading: false
+          });
+        })
+        .catch(error => {
+          this.setState({
+            error: error,
+            loading: false
+          });
+        });
+    }
+  
+    render() {
+      const { loading, error, books } = this.state;
+  
+      if (loading) return <div>Loading books...</div>;
+      if (error) return <div>Error loading books: {error.message}</div>;
+  
+      return (
+        <div>
+          <h1>Library</h1>
+          <div className="book-list">
+            {books.map((book, index) => (
+              <Book key={index} {...book} />
+            ))}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  export default LibraryPage;
