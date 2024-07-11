70013a8e6848ce5d2efb21e41aaf37fd19a0f96c
Jake Lovitz <jakelovitz@Pipes-MacBook-Pro.local>
2024-07-10 23:20:27 -0400

    updated content

--- src/assets/favoriteThings.js
+++ src/assets/favoriteThings.js
@@ -3,34 +3,32 @@ import images from '../utils/importImages';
 const favoriteThings = [
   {
     name: 'Movies',
-    blurb:
-      'These are the films I always find my way back to, and know will continue to grace my screen for many years to come.',
+    blurb: `I'm not a fervent film aficionado, as one can probably discern from the fact that these are the films I somehow can't resist continously popping on.`,
     items: [
       {
-        text: 'Star Wars Episode V',
-        description: `The quintessential Star Wars, Empire Strikes Back is my favorite of the franchise. From the opening battle on Hoth, the dual narrative of Luke's training alongside Han and Leia's flight, and culminating with the disasters in Cloud City`,
-        image: images['empire_strikes_back.jpeg'],
+        text: 'The Birdcage',
+        description: `"Oh, yes. Another jibe, another joke at my expense. Well, why not? I'm not young, I'm not new, and everyone laughs at me. I'm quite aware of how ridiculous I am. The only solution is to go to where nobody is ridiculous, and everyone is equal." — Albert Goldman. The Bird`,
+        image: images['the_birdcage.png'],
+      },
+      {
+        text: 'My Dinner with Andre',
+        description: `My introduction to this movie was the Dinner with Andre action figures at the tail end of Waiting for Guffman. It took me no small amount of time to realize that the man behind the titular pronoun was not only the screenwriter of this movie specifically and plays more generally, he was the guy who kept saying 'Inconcievable!' in the Princess Bride. Once said realization came upon me, there was nothing doing until I watched the movie, which I've then proceeded to do time and again. While the dialogue often veers into monologue (particularly in the first half) the details and reservations in Andre Gregory's performance add a depth that changes my opinion of his character (and thus the film) each time I watch it. The conversation, scripted based on weeks of recorded conversations between Shawn and Gregory, is loose but lovely, contrasting without being combative. Perhaps my favorite touch, though, is how the movie is Satie''Gymnopédies', pushing you into a quiet reflection of Wally's Dinner with Andre.`,
+        image: images['dinner_four.png'],
       },
       {
         text: 'The Shawshank Redemption',
-        description: `Redemption`,
+        description: `While I appreciate Morgan Freeman's work in almost everything, 'The Shawshank Redemption' has always been my favorite Freeman vehicle. Despite the grimness that comprises the bulk of the film, the Stoic Theme leading the intro and one of the loveliest endings in cinematic history ensure 'The Shawshank Redemption' stuns me every time.`,
         image: images['shawshank.png'],
       },
       {
         text: 'The Big Lebowski',
-        description: `I am a guy of a certain age so it's probably not all that surprising this one made the cut.`,
+        description: `I actually struggle to articulate why I've always loved this movie so much, but I am a guy of a certain age so I suppose it's probably not all that surprising.`,
         image: images['lebowski.png'],
       },
       {
-        text: 'My Dinner with Andre',
-        description:
-          'Every time I watch this movie I fixate on something different, which provides a whole new lens through which to view the entire film.',
-        image: images['dinner_four.png'],
-      },
-      {
-        text: 'The Birdcage',
-        description: `"Oh, yes. Another jibe, another joke at my expense. Well, why not? I'm not young, I'm not new, and everyone laughs at me. I'm quite aware of how ridiculous I am. The only solution is to go to where nobody is ridiculous, and everyone is equal." — Albert Goldman ${(<br />)} If the line above strikes you as out of place in a comedy, please go watch Nathan Lane and Robin Williams transorm the `,
-        image: images['the_birdcage.png'],
+        text: 'Star Wars Episode V',
+        description: `The quintessential Star Wars movie (great spaceship scenes, lightsaber fights, and sassy droids), 'The Empire Strikes Back' is my favorite of the franchise.`,
+        image: images['empire_strikes_back.jpeg'],
       },
     ],
   },
@@ -40,23 +38,28 @@ const favoriteThings = [
     items: [
       {
         text: 'Typhoon - White Lighter',
-        description: 'Description for Item 1',
+        description: `When I first heard 'Artificial Fathers' two years after it came out, it was jaw-dropping. Even more surprising was discovering later that day that it wasn't even the best song on the album. I don't think I've ever found another record quite like 'White Lighter,' but it hasn't been for lack of trying.`,
         image: images['white_lighter.png'],
       },
       {
-        text: 'London Calling - The Clash',
-        description: 'Description for Item 2',
-        image: images['london_calling.png'],
+        text: 'Tim: Let It Bleed Edition - The Replacements',
+        description: `The original version of 'Tim' is incredible on its own, but the pop and crackle of Ed Stasium's remaster adds nuance and dynamism, making it clear why the 'Mats are among the greatest rockers America ever produced.`,
+        image: images['let_it_bleed.png'],
       },
       {
         text: 'Unplugged in New York - Nirvana',
-        description: 'Description for Item 2',
+        description: `I could probably start off every day listening to this album, but to experience it fully, I'd strongly recommend finding any way to watch the concert in its entirety. Fourteen songs, six of which are covers, all showcase the depths of musicality Nirvana drew from, even if that's not always immediately apparent from tracks like 'Radio Friendly Unit Shifter.' In the latter half, the Kirkwood Brothers join to play three Meat Puppets songs before the last Nirvana original of the show, 'All Apologies.' The concert ends with a hair-raising, heartbreaking cover of 'Where Did You Sleep Last Night' that hits particularly hard after spending the last 50 minutes right in front of that stage.`,
         image: images['unplugged.png'],
       },
       {
-        text: 'Tim: Let It Bleed Edition - The Replacements',
-        description: 'Description for Item 2',
-        image: images['let_it_bleed.png'],
+        text: 'Double Nickels on the Dime - Minutemen',
+        description: `1984 was a standout year for the American alternative scene: The Replacements' 'Let It Be,' Hüsker Dü's 'Zen Arcade,' Black Flag's 'My War,' and the Meat Puppets' eponymous second album, 'II,' just to name a few. Of the lot, though, I'll always keep coming back to 'Double Nickels on the Dime.`,
+        image: images['nickels.jpg'],
+      },
+      {
+        text: 'London Calling - The Clash',
+        description: `Rudie can't fail`,
+        image: images['london_calling.png'],
       },
     ],
   },
@@ -71,43 +74,39 @@ const favoriteThings = [
       },
       {
         text: 'Gengar',
-        description: 'Description for Item 2',
+        description: `For this shady (ha) little Ghost/Poison type with a big Cheshire cat grin it was aand remains worth it to track down a buddy willing to go through a trade and back in order to upgrade a Haunter to a Gengar.`,
         image: images['gengar.png'],
       },
       {
         text: 'Arcanine',
-        description: 'Description for Item 2',
+        description: `A conundrum of the first game was my burning (ha) desire to have both a Charizard and an Arcanine on my team, despite the superfluousness of that decision. Nowadays I would force myself to choose between them for the sake of balance, and in a one-to-one match up I'd have to Arcanine, because I really like dogs and Charizard looks like a meaner Dragonite, which I'll always have.`,
         image: images['arcanine.png'],
       },
       {
-        text: 'Alakazam',
-        description: 'Description for Item 2',
-        image: images['alakazam.png'],
-      },
-      {
         text: 'Ampharos',
-        description: 'Description for Item 2',
+        description:
+          'One of the few non-first Generation pokemon I always try to get my hands on. I caught a Mareep on first sight when initially playing Silver version as a youth, if only for an appreciation of sheep, but my face really lit up (ha) when it eventually reached its final form.',
         image: images['ampharos.png'],
       },
       {
         text: 'Mewtwo',
-        description: `Over-use of my Mewtwo at the age of 7 led to a level 100 terror and I've remained fond of Mewtwo ever since`,
+        description: `Over-use of this 'Mon at the age of 7 led to a level 100 terror and I've remained fond of Mewtwo ever since.`,
         image: images['mewtwo.png'],
       },
     ],
   },
   {
     name: 'Spaceships',
-    blurb: 'eyo spaceships',
+    blurb: `As most fan of the genre would likely agree, it's hard not to indulge in Science Fiction (across mediums) and not pick up a few favorite ships.`,
     items: [
       {
         text: 'The Millenium Falcon',
-        description: `The spacecraft personified: Modified Corellian Freighter, YT-1300.`,
+        description: `The spacecraft personified: Modified Corellian Freighter, YT-1300. The connection between a Captain or Crew and their ship, has been explored time and again, but the Falcon always struck me as the third part of a three legged stool — every bit as fundamental part of the trio as Han and Chewbacca.`,
         image: images['falcon_gold.png'],
       },
       {
         text: 'The Enteprise D',
-        description: 'TNG started ',
+        description: `Scotty may have demanded to see the bridge of the "Entperprise, no bloody A, B, C, or D", but I would pick the NCC-1701-D every time.`,
         image: images['underside_d.png'],
       },
       {
@@ -125,11 +124,11 @@ const favoriteThings = [
   },
   {
     name: 'Books',
-    blurb: `My favorite of the genre broadly.`,
+    blurb: `As you might be able to guess from my Library page, I rather enjoy books and reading, so much so I had to find some constraint on this list. I generally gravitate more towards non-fiction, but the times I don't I'll opt for the truly fantastic. Here are some of my favorite books of Speculative Fiction, broadly.`,
     items: [
       {
         text: `Hitchhiker's Guide to the Galaxy`,
-        description: 'Description for Item 1',
+        description: `Description for Hitchhiker's guide`,
         image: images['fortytwo.jpg'],
       },
       {
@@ -139,8 +138,7 @@ const favoriteThings = [
       },
       {
         text: 'The Scar',
-        description:
-          'Technically but not really a sequel to his also terrific Perdido Street Station, The Scar stands out as my favorite Mieville book (a distinction that perhaps only matters.',
+        description: `description for the scar`,
         image: images['the_scar.jpg'],
       },
       {
